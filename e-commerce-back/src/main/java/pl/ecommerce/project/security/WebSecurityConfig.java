c2e6b2ba1b162eab5bc3eddca87badb4a7871fed
Daniel <daniel.strielnikow@outlook.com>
2025-05-17 00:23:23 +0200

    JWT2

--- e-commerce-back/src/main/java/pl/ecommerce/project/security/WebSecurityConfig.java
+++ e-commerce-back/src/main/java/pl/ecommerce/project/security/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package pl.ecommerce.project.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,19 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import pl.ecommerce.project.model.Role;
-import pl.ecommerce.project.model.User;
-import pl.ecommerce.project.model.app.AppRole;
 import pl.ecommerce.project.repo.RoleRepository;
 import pl.ecommerce.project.repo.UserRepository;
 import pl.ecommerce.project.security.jwt.AuthEntryPointJwt;
 import pl.ecommerce.project.security.jwt.AuthTokenFilter;
 import pl.ecommerce.project.security.services.UserDetailsServiceImpl;
-
-import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
